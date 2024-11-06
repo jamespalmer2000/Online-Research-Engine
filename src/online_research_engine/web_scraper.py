89fa692b681c153c433168d9d3fc305c8baafc23
James Palmer <james.palmer@doosan.com>
2024-11-06 16:13:36 -0600

    feat: Playwright web scraper
    - scrape web page html after rendering all JavaScript content

--- src/online_research_engine/web_scraper.py
+++ src/online_research_engine/web_scraper.py
@@ -2,6 +2,8 @@ import re
 
 import requests
 from bs4 import BeautifulSoup
+from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
+from playwright.sync_api import sync_playwright
 
 
 class WebScraper:
@@ -50,6 +52,51 @@ class WebScraper:
     def convert_html_to_soup(self, html):
         # Convert the HTML string to a BeautifulSoup object for parsing
         html_string = html.text
+        return BeautifulSoup(html_string, "lxml")
+
+    def extract_main_content(self, html_soup, rule=0):
+        # Extract the main content from a BeautifulSoup object
+        main_content = []
+        tag_rule = re.compile("^(h[1-6]|p|div)" if rule == 1 else "^(h[1-6]|p)")
+        # Iterate through specified tags and collect their text
+        for tag in html_soup.find_all(tag_rule):
+            tag_text = tag.get_text().strip()
+            if tag_text and len(tag_text.split()) > 10:
+                main_content.append(tag_text)
+        return "\n".join(main_content).strip()
+
+    def scrape_url(self, url, rule=0):
+        # Public method to scrape a URL and extract its main content
+        webpage_html = self.get_webpage_html(url)
+        soup = self.convert_html_to_soup(webpage_html)
+        main_content = self.extract_main_content(soup, rule)
+        return main_content
+
+
+class PlaywrightWebScraper:
+    def __init__(self):
+        return
+
+    def get_webpage_html(self, url):
+        # NOTE: unlike WebScraper.get_webpage_html,
+        # this function returns the response text, not the response object itself
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            try:
+                response = page.goto(url)
+                response.finished()
+            except PlaywrightTimeoutError:
+                return ""
+
+            page_content = page.content()
+            browser.close()
+
+            return page_content
+
+    def convert_html_to_soup(self, html):
+        # Convert the HTML string to a BeautifulSoup object for parsing
+        html_string = html
         return BeautifulSoup(html_string, "lxml")
 
     def extract_main_content(self, html_soup, rule=0):
--- tests/integration/test_web_scraper.py
+++ tests/integration/test_web_scraper.py
@@ -1,6 +1,6 @@
 import unittest
 
-from online_research_engine.web_scraper import WebScraper
+from online_research_engine.web_scraper import PlaywrightWebScraper, WebScraper
 
 
 class TestWebScraper(unittest.TestCase):
@@ -8,6 +8,20 @@ class TestWebScraper(unittest.TestCase):
         scraper = WebScraper(user_agent="macOS")
         test_url = "https://en.wikipedia.org/wiki/Apple_Inc."
         main_content = scraper.scrape_url(test_url)
+        print(main_content)
+
+
+class TestPlaywrightWebScraper(unittest.TestCase):
+    def test_get_webpage_html(self):
+        scraper = PlaywrightWebScraper()
+        test_url = "https://www.apple.com"
+        webpage_html = scraper.get_webpage_html(test_url)
+        print(webpage_html)
+
+    def test_web_scraper(self):
+        scraper = PlaywrightWebScraper()
+        test_url = "https://www.apple.com"
+        main_content = scraper.scrape_url(test_url, rule=1)
         print(main_content)
 
 
